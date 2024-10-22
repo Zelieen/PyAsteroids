79580453b1b61cb5f067cdd906b48f42dadef41a
Zelieen <firina@gmx.net>
2024-10-22 18:11:09 +0200

    add player movement, add containers

--- main.py
+++ main.py
@@ -11,16 +11,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
-    #spawn player in the middle of the screen
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-
-    while True:
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2) #spawn player in the middle of the screen
+    
+    while True: #the game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for obj in updatable:
+            obj.update(dt)
+
+        #render game
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
         #linit game to 60 FPS
